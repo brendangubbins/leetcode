1aecc3f8056824dfadd169476fb99e997c6c24cb
brendangubbins <brendansg97@gmail.com>
2021-07-14 16:07:09 -0400

    running sum, middle of linked list

---
+++ 0876 - Middle of the Linked List/MiddleOfTheLinkedList.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode copy = head;
+        int length = 0;
+        
+        while (copy != null) {
+            copy = copy.next;
+            length++;
+        }
+        
+        for (int i = 0; i < length / 2; i++) {
+            head = head.next;    
+        }
+        
+        return head;
+    }
+}
