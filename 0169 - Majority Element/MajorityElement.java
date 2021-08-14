62ee7269e5a3b73a9c751b2610fb11344ec2cedb
brendangubbins <brendansg97@gmail.com>
2021-08-14 00:20:46 -0400

    lc 169

---
+++ 0169 - Majority Element/MajorityElement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.get(key) > Math.floor(nums.length / 2))
+                return key;
+        }
+        return 0;
+    }
+}
